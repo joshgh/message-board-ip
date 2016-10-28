9a8fcf7f2d063759845aa2901941e1ff6bb0692b
Joshua Huffman <j.m.huffman@gmail.com>
2016-10-28 09:12:34 -0700

    add ability to create new questions and show question previews on index page

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  newQuestionFormIsShowing: false,
+  actions: {
+    showForm() {
+      this.set('newQuestionFormIsShowing', true);
+    },
+
+    saveQuestion() {
+      var params = {
+        author: this.get('author'),
+        content: this.get('content'),
+        notes: this.get('notes')
+      };
+      this.sendAction('saveQuestion', params);
+    }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('question');
+  },
+  actions: {
+    saveQuestion(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+    }
+  }
 });
