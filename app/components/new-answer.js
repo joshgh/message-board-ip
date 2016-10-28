48cf9a203a673a14bebbdb99607983399c5caf63
Joshua Huffman <j.m.huffman@gmail.com>
2016-10-28 10:32:42 -0700

    add ability to answer questions and show answers

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  newAnswerFormIsShowing: false,
+  actions: {
+    showAnswerForm() {
+      this.set('newAnswerFormIsShowing', true);
+    },
+    saveAnswer() {
+      var params = {
+        author: this.get('author'),
+        content: this.get('content'),
+        question: this.get('question')
+      };
+      this.set('newAnswerFormIsShowing', false);
+      this.sendAction('saveAnswer', params);
+    }
+  }
 });
