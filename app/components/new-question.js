7ed90ac91c10cee42446ac36d169a8afbcdfaf87
Joshua Huffman <j.m.huffman@gmail.com>
2016-11-04 11:01:04 -0700

    require a user to login to post a question, get author from username instead of input at time question is asked

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,23 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  user: Ember.inject.service(),
   newQuestionFormIsShowing: false,
   actions: {
     showForm() {
-      this.set('newQuestionFormIsShowing', true);
+        this.set('newQuestionFormIsShowing', true);
     },
     hideForm() {
       this.set('newQuestionFormIsShowing', false);
     },
     saveQuestion() {
       var params = {
-        author: this.get('author') || "anonymous",
+        author: this.get('user').get('userName'),
         content: this.get('content') || "",
         notes: this.get('notes') || "none",
         score: 0,
         timestamp: new Date().toISOString()
       };
-      this.set('author', "");
       this.set('content', "");
       this.set('notes', "");
       this.set('newQuestionFormIsShowing', false);
