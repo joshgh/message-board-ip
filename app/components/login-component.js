17c2184d437dc750a99e259e568bf34951214e2e
Joshua Huffman <j.m.huffman@gmail.com>
2016-11-04 14:17:33 -0700

    require username field for login

--- app/components/login-component.js
+++ app/components/login-component.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  name: "",
   user: Ember.inject.service(),
+  isInvalid: Ember.computed('name', function() {
+    let patt = /^$|^\s+$/;
+    return patt.test(this.get('name'));
+  }),
   actions: {
     login(){
       this.get('user').login(this.get('name'));
